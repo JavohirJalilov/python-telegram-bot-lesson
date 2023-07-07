8f6d1783ae045dd25376bf15a241b27853c6aade
JavohirJalilov <javohirjaliloff@gmail.com>
2023-07-07 10:09:32 +0500

    command handler

--- main.py
+++ main.py
@@ -1,18 +1,26 @@
-from telegram.ext import CallbackContext, Updater, MessageHandler, Filters
+from telegram.ext import CallbackContext, Updater, MessageHandler, Filters, CommandHandler
 from telegram import Update
 
 import os 
 
 TOKEN = os.environ['TOKEN']
 
+def start(update: Update, context: CallbackContext):
+    chat_id = update.message.chat.id
+    bot = context.bot
+
+    bot.sendMessage(chat_id, "Bizning botga hush kelibsiz!")
 
 def text(update: Update, context: CallbackContext):
+    bot = context.bot
     message = update.message.text
     chat_id = update.message.chat.id
-    print(message, chat_id)
+
+    bot.sendMessage(chat_id=chat_id, text=message)
 
 updater = Updater(token=TOKEN)
 
+updater.dispatcher.add_handler(CommandHandler('start', start))
 updater.dispatcher.add_handler(MessageHandler(Filters.text, text))
 
 updater.start_polling()
