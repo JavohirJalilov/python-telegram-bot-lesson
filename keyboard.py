6c1bf3521bad4e62d29a4a5bd38c5e31e368bbc0
JavohirJalilov <javohirjaliloff@gmail.com>
2023-07-10 10:02:59 +0500

    create reply keyboard

---
+++ keyboard.py
@@ -0,0 +1,22 @@
+import telegram
+from telegram import ReplyKeyboardMarkup, KeyboardButton
+import os 
+
+TOKEN = os.environ["TOKEN"]
+
+bot = telegram.Bot(TOKEN)
+
+button1 = KeyboardButton(text='Button1')
+button2 = KeyboardButton(text='Button2')
+
+buttons = [
+    [button1, button2]
+]
+keyboard = ReplyKeyboardMarkup(buttons, resize_keyboard=True)
+
+update = bot.getUpdates()[-1]
+chat_id = update.message.chat.id
+
+msg = bot.sendMessage(chat_id, "Keyboard", reply_markup = keyboard)
+
+print(msg)
